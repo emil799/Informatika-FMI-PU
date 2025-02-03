6112308b26e70cfeac2a3a8bb3f1d0fdf024dded
Емил Медаров <110677472+emil799@users.noreply.github.com>
2025-02-04 00:25:12 +0200

    Update MainClass.java

--- Университет/1-ви курс/Програмиране на Java/Упражнения/ExampleWithClassesMethods&Constructors/src/student/examples/MainClass.java
+++ Университет/1-ви курс/Програмиране на Java/Упражнения/ExampleWithClassesMethods&Constructors/src/student/examples/MainClass.java
@@ -3,10 +3,10 @@ package student.examples;
 public class MainClass {
 
 	public static void main(String[] args) {
-		Student s1=new Student("Ivan","4812062366",72);
-		Student s2=new Student("Emil","0142252344",20);
+		Student s1=new Student("Ivan","0000000000",72);
+		Student s2=new Student("Emil","0000000000",20);
 		s2.moveToNextGrade();
-		s1.setEgn("0142252344");
+		s1.setEgn("0000000000");
 		System.out.println(s1.printInfo());
 		System.out.println(s2.printInfo());
 
